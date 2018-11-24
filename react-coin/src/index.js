bdca7c3d31bb2b67a0bae876425218e11891d94d
Charles Mendes <contato@charlesmendes.com>
2018-11-24 14:53:59 -0200

    Componentes UI com JSX

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './components/common/Header'
 
 const App = () => {
-    return <h1>React Coin</h1>
+    const title = 'React Coin'
+
+    return (
+        <div>
+            <Header />
+            <h1>{ title }</h1>
+
+            <p>Exibe as cotações de criptomoedas</p>
+        </div>
+    )
 }
 
 ReactDOM.render(
